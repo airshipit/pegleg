152b42bc3b32a624ed0748ceef1a911f268d9956
Tin Lam <tin@irrational.io>
2018-11-12 02:26:22 -0600

    Consolidate linter jobs
    Consolidating all the linting to a single job rather than having a one-off
    just to lint for trailing whitespaces. As most of this projects are python,
    this should already be covered by PEP8, but will be scanning for yamls with
    trailing whitespaces.
    
    Change-Id: Iee33a69ff234d21c08217faa33a19e11dfef0ad9
    Signed-off-by: Tin Lam <tin@irrational.io>

---
+++ tools/gate/whitespace-linter.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -xe
+RES=$(find . \
+  -not -path "*/\.*" \
+  -not -path "*/doc/build/*" \
+  -not -path "*/doc/source/images/*" \
+  -not -name "*.tgz" \
+  -type f -exec egrep -l " +$" {} \;)
+
+if [[ -n $RES ]]; then
+  exit 1;
+fi
