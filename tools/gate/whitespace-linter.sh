e3f6efbb1f90541066d6daed854ad89bce340afd
Felipe Monteiro <felipe.monteiro@att.com>
2018-11-25 12:30:56 -0500

    trivial: fix whitespace-linter catching false positives
    Ignores .pyc files and files contained in htmlcov as a coverage
    job was recently added.
    
    Change-Id: I33b1b88e90060f3b26f41b6acbbc0c7091caeb57

--- tools/gate/whitespace-linter.sh
+++ tools/gate/whitespace-linter.sh
@@ -5,7 +5,9 @@ RES=$(find . \
   -not -path "*/\.*" \
   -not -path "*/doc/build/*" \
   -not -path "*/doc/source/images/*" \
+  -not -path "*/htmlcov/*" \
   -not -name "*.tgz" \
+  -not -name "*.pyc" \
   -type f -exec egrep -l " +$" {} \;)
 
 if [[ -n $RES ]]; then
