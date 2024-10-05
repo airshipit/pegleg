457b10ad13e40441ae916290d414e38602497483
Sergiy Markin <smarkin@mirantis.com>
2024-10-05 14:50:50 +0000

    Treasuremap references update
    Also fixed readthedocs publishing and pumped up
    docker Pypi module version and updated docs
    generation code caused by deprecated properties in
    recent version of Sphinx.
    
    Change-Id: Idc69d0e6a06ba6d931acacbd22df076d01901ed0

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -20,7 +20,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../../'))
-import sphinx_rtd_theme
 
 
 # -- General configuration ------------------------------------------------
@@ -89,7 +88,6 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
